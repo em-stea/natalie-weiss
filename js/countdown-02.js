5a4ff8a6107a47a83de14e62f64249c473b6f619
em-stea <emiliastea@hotmail.com>
2022-07-11 10:27:50 -0300

    .upd

--- js/countdown-02.js
+++ js/countdown-02.js
@@ -1,7 +1,7 @@
 simplyCountdown("#countdown", {
   year: 2022, // required
   month: 7, // required
-  day: 18, // required
+  day: 23, // required
   hours: 15, // Default is 0 [0-23] integer
   minutes: 3, // Default is 0 [0-59] integer
   seconds: 0, // Default is 0 [0-59] integer
